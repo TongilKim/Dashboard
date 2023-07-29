75aa2b749fc39fa2bb0df3c23426250d4077cc89
Tongil Kim <kkimtt1234@nntuple.com>
2023-07-29 22:22:12 +0900

    implement api

---
+++ src/apis/index.tsx
@@ -0,0 +1,38 @@
+type EventCountData = {
+  data: Data;
+  result: boolean;
+  message: string;
+  version: string;
+  last_compile_time: string;
+};
+type Data = {
+  headers: Header[];
+  rows: string[][];
+};
+type Header = {
+  idx: number;
+  key: string;
+  label: string;
+  description: string;
+  property_type: string;
+  value_type: string;
+};
+
+export const getEventCountApi = async () => {
+  let data: EventCountData | null = null;
+
+  try {
+    // baseURL is configured in webpack
+    await fetch(`https://static.adbrix.io/front/coding-test/event_1.json`, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        data = res;
+      });
+  } catch (error) {
+    // should be implemented with some error cases or something
+    data = null;
+  }
+  return data;
+};
